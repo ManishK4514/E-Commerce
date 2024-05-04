01ae50ee1c49c328f12843ed274bceedf9fc60c7
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2024-05-04 23:55:56 +0530

    testing Navbar scroller issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
     return (
       <>
           <Navbar />
-          <div className="flex w-screen h-screen bg-[#f2f0f1]">
+          <Home />
+          {/* <div className="flex w-screen h-screen bg-[#f2f0f1]">
 
-          </div>
+          </div> */}
       </>
     );
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import rectangle from "../assets/rectangleLanding.svg";
+
+const Home = () => {
+  return (
+    <div className="flex w-screen h-screen bg-[#f2f0f1]" >
+        <img src={rectangle} alt="" srcset="" />
+    </div>
+  )
+}
+
+export default Home
