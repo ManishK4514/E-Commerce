7660aef8bb39aef02e2abe13d509028569af9211
sailendrachettri <sailendra9083@gmail.com>
2024-05-06 21:28:21 +0530

    product tentitive name added

--- client/src/components/pages/Products.jsx
+++ client/src/components/pages/Products.jsx
@@ -9,39 +9,43 @@ const Products = () => {
     return (
         <>
             <h1 className='text-center font-black text-5xl py-16 text-slate-600'>Explore the products</h1>
-            <div className='container-fluid flex justify-center'>
+            <div className='container-fluid flex justify-center my-4'>
                 <div class="grid grid-cols-3 gap-4 text-center">
                     <div>
                         <img className='max-h-60 my-2 mx-2' src={produc1} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
+                        <p className="text-lg text-blue-600 ">MILITARY BLUEAIR JORDAN IV</p>
                     </div>
                     <div>
                         <img className='max-h-60 my-2 mx-2' src={produc2} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
+                        <p className="text-lg text-blue-600 ">ADIDAS EURO STYLE CUP 2024</p>
                     </div>
                     <div>
                         <img className='max-h-60 my-2 mx-2' src={produc3} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
-                    </div>
-                    <div>
-                        <img className='max-h-60 my-2 mx-2' src={produc4} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
-                    </div>
-                    <div>
-                        <img className='max-h-60 my-2 mx-2' src={produc3} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
-                    </div>
-                    <div>
-                        <img className='max-h-60 my-2 mx-2' src={produc2} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
+                        <p className="text-lg text-blue-600 ">STYLE GUIDE: SHIRTS</p>
                     </div>
                     <div>
                         <img className='max-h-60 my-2 mx-2' src={produc1} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
+                        <p className="text-lg text-blue-600">DINGYUN ZHANG X ADIDAS SAMBA</p>
                     </div>
                     <div>
                         <img className='max-h-60 my-2 mx-2' src={produc4} alt="Products" />
-                        <p>Lorem, ipsum dolor.</p>
+                        <p className="text-lg text-blue-600 ">NATURAL TECH</p>
+                    </div>
+                    <div>
+                        <img className='max-h-60 my-2 mx-2' src={produc3} alt="Products" />
+                        <p className="text-lg text-blue-600 ">ADIDAS EURO STYLE CUP 2024</p>
+                    </div>
+                    <div>
+                        <img className='max-h-60 my-2 mx-2' src={produc2} alt="Products" />
+                        <p className="text-lg text-blue-600 ">STYLE GUIDE: SHIRTS</p>
+                    </div>
+                    <div>
+                        <img className='max-h-60 my-2 mx-2' src={produc1} alt="Products" />
+                        <p className="text-lg text-blue-600 ">MILITARY BLUEAIR JORDAN IV</p>
+                    </div>
+                    <div>
+                        <img className='max-h-60 my-2 mx-2' src={produc4} alt="Products" />
+                        <p className="text-lg text-blue-600 ">NATURAL TECH</p>
                     </div>
                 </div>
             </div>
